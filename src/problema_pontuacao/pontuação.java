f613d738572b11b100fd29a1319fef146232087a
Guilherme Staciarini <staciarinineto@gmail.com>
2020-05-15 19:27:36 -0300

    Cabecalho

---
+++ src/problema_pontuacao/pontuação.java
@@ -0,0 +1,22 @@
+package problema_pontuacao;
+
+//bibliotecas
+import java.util.Locale;
+import java.util.Scanner;
+
+public class pontuação {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US); // idioma US
+		Scanner sc = new Scanner(System.in); // leitura de dados
+		
+		// CABEÇALHO
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("-----------------------------------");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		
+
+	}
+
+}
