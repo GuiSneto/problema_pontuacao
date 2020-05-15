199c75182663f3374fefafb6e79eb46233d58fd2
Guilherme Staciarini <staciarinineto@gmail.com>
2020-05-15 19:49:02 -0300

    Leitura dos outros dados

--- src/problema_pontuacao/pontuação.java
+++ src/problema_pontuacao/pontuação.java
@@ -18,14 +18,30 @@ public class pontuação {
 		System.out.println("");
 		System.out.println("");
 		
-		
-		int VOLUMEC;
+		// Declaração das variaveis
+		int VOLUMEC,ATRPAG;
 		double TICKETM;
+		char FORMPAG;
 		
+		
+		// Leitura dos dados
 		System.out.print("Quantas compras o cliente fez no último ano? ");
 		VOLUMEC = sc.nextInt();
 		System.out.print("Qual o ticket médio? ");
 		TICKETM = sc.nextDouble();
+		System.out.println("");
+		System.out.println("");
+		
+		
+		System.out.print("Quantas vezes o cliente atrasou o pagamento? ");
+		ATRPAG = sc.nextInt();
+		System.out.print("A maioria das compras foi em dinheiro, cartão, ou boleto (D/C/B)? ");
+		FORMPAG = sc.next().charAt(0);
+		System.out.println("");
+		System.out.println("");
+		
+		
+		
 		
 		
 		
