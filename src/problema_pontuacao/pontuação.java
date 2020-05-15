4d732eae82ea1d4576fe88695cc3c7c79807e9a0
Guilherme Staciarini <staciarinineto@gmail.com>
2020-05-15 20:05:51 -0300

    Score de volume de compras

--- src/problema_pontuacao/pontuação.java
+++ src/problema_pontuacao/pontuação.java
@@ -19,7 +19,7 @@ public class pontuação {
 		System.out.println("");
 		
 		// Declaração das variaveis
-		int VOLUMEC,ATRPAG;
+		int VOLUMEC,ATRPAG,SCOREVOLUMEC = 0;
 		double TICKETM;
 		char FORMPAG;
 		
@@ -32,13 +32,35 @@ public class pontuação {
 		System.out.println("");
 		System.out.println("");
 		
-		
+		//Leitura de outros dados
 		System.out.print("Quantas vezes o cliente atrasou o pagamento? ");
 		ATRPAG = sc.nextInt();
 		System.out.print("A maioria das compras foi em dinheiro, cartão, ou boleto (D/C/B)? ");
 		FORMPAG = sc.next().charAt(0);
 		System.out.println("");
 		System.out.println("");
+		
+		//calculo score volume
+		
+		if (VOLUMEC == 0) {
+			SCOREVOLUMEC = 0;
+		}
+		else if (VOLUMEC*TICKETM <= 3000 && VOLUMEC <= 2) {
+			SCOREVOLUMEC = 20;
+		}
+		else if (VOLUMEC*TICKETM <= 3000 && VOLUMEC > 2) {
+			SCOREVOLUMEC = 40;
+		}
+		else if (VOLUMEC*TICKETM > 3000) {
+			SCOREVOLUMEC = 60 ;
+		}
+		
+		System.out.println("Score de volume de compras = " + SCOREVOLUMEC);
+		System.out.println("");
+		System.out.println("");
+		
+		
+		
 		
 		
 		
